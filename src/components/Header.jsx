bda872beeb8c17da5e0b806218b84b62e790aedd
Yuvraj <kingyuvi88@gmail.com>
2023-10-09 12:45:17 +0530

    Created Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+const Header = ()=>{
+    return(
+        <>
+        <div class="head1">
+            <table cellspacing="15">
+                <tr>
+                    <td>
+                        <img class="logo" src="./logo.png" alt="vital vishwa publications logo"/>
+                    </td>
+                    <td>
+                        <h1 class="title">Vital Vishwa Publications</h1>
+                        <p>Nurturing Ideas, Crafting Realities</p>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        </>
+    )
+}
+export default Header; 
