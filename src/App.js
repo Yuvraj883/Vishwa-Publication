ff18f9c7f04dd005e2b9cfac79ced1d27878f05a
Yuvraj <kingyuvi88@gmail.com>
2023-11-16 05:15:55 +0530

    Added Carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
+import React, { useState, useEffect } from 'react';
+
+
 
 
 function App() {
+
+
+// Import all images from the 'galleryImages' folder with a filename pattern
+// const imageContext = require.context('!file-loader!../public/galleryImages', false, /\.(jpg|jpeg|png)$/);
+// const images = imageContext.keys().map((path) => imageContext(path).default || imageContext(path));
+
+const images = [
+  '/galleryImages/img1.png',
+  '/galleryImages/img2.png',
+  '/galleryImages/img3.png'
+
+]
+
   const [loader, setLoader] = useState(true);
+  console.log(images[0]);
 
  useEffect(()=>{
   setTimeout(()=>{
@@ -22,13 +40,15 @@ function App() {
     <div>
       <Header></Header>
    <Navbar></Navbar>
+   <Carousel images={images}></Carousel>
+   
 
     </div>
   }
   {
     loader &&
-    <div>
-      Sex sex sex
+    <div className=' flex flex-col justify-center items-center h-[100%] text-3xl '>
+      <p>Loading...</p>
     </div>
   }
    
