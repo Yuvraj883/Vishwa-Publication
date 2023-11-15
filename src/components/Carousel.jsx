ac061adc38d915af6b9aea6703ebc677b8c2c94b
Yuvraj <kingyuvi88@gmail.com>
2023-11-16 05:13:33 +0530

    Updated Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,67 @@
-const Carousel = ()=>{
-    return(
-        <>
-        
-        </>
-    )
-}
+import React, { useState, useEffect } from 'react';
+
+const Carousel = ({ images }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev - 1 + images.length) % images.length);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      nextSlide();
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [currentSlide]);
+
+  return (
+    <div className="relative w-[90%] h-[70%] mx-auto mt-8 overflow-hidden bg-white rounded-lg shadow-md">
+      <div
+        className="flex items-center justify-center transition-transform duration-500 ease-in-out"
+        style={{ transform: `translateX(-${currentSlide * 100}%)`, width: `${images.length}00%`, height: '70%' }}
+      >
+        {images.map((image, index) => (
+          <div key={index} className="w-full flex-shrink-0">
+            <img
+              src={image}
+              alt={`Slide ${index + 1}`}
+              className=" object-cover rounded-lg"
+            />
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={prevSlide}
+        className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full focus:outline-none z-10"
+      >
+        &lt;
+      </button>
+      <button
+        onClick={nextSlide}
+        className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full focus:outline-none z-10"
+      >
+        &gt;
+      </button>
+      <div className="flex justify-center mt-4">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => setCurrentSlide(index)}
+            className={`w-4 h-4 mx-1 rounded-full cursor-pointer ${
+              currentSlide === index ? 'bg-gray-800' : 'bg-gray-400'
+            } transition-all duration-300`}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
