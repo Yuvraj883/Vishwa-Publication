539ce66868b96b28d00be879b1fcfa088b73dac0
Yuvraj <kingyuvi88@gmail.com>
2023-11-16 15:52:34 +0530

    Added EminentPersonalities

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Authors from './components/Authors';
 import WhatsApp from './components/Whatsapp';
 import { css } from '@emotion/react';
 import { BarLoader } from 'react-spinners';
+import EminentPersonalities from './components/EminentPersonalities';
 
 function App() {
   const images = [
@@ -70,6 +71,23 @@ function App() {
   
   ]
 
+  const eminentPersonalities = [
+     '/eminentPersonalities/ep1.png',
+     '/eminentPersonalities/ep2.png',
+     '/eminentPersonalities/ep3.png',
+     '/eminentPersonalities/ep4.png',
+     '/eminentPersonalities/ep5.png',
+     '/eminentPersonalities/ep6.png',
+     '/eminentPersonalities/ep7.png',
+     '/eminentPersonalities/ep8.png',
+     '/eminentPersonalities/ep9.png',
+     '/eminentPersonalities/ep10.png',
+     '/eminentPersonalities/ep11.png',
+     '/eminentPersonalities/ep12.png',
+
+
+  ]
+
   const [loader, setLoader] = useState(true);
 
   useEffect(() => {
@@ -99,6 +117,7 @@ function App() {
           <Books images={books} />
           <Authors images={authors} />
           <WhatsApp phoneNumber={9999762706} />
+          <EminentPersonalities images={eminentPersonalities}></EminentPersonalities>
         </div>
       )}
     </>
