aa8af38956d032e21078e2d5ae7da63faa0d6bd5
Yuvraj <kingyuvi88@gmail.com>
2023-11-17 03:26:30 +0530

    Updated style

--- src/components/BestSelling.jsx
+++ src/components/BestSelling.jsx
@@ -1,12 +1,13 @@
 const BestSelling = ()=>{
     return(
         <>
-          <div className="bg-black flex flex-row justify-between px-8 text-white  py-4 mt-2 w-screen ">
+          <div className="bg-black flex flex-row justify-between mx-auto px-8 text-white  py-6 mt-8 mb-8 w-[90%] rounded-lg ">
           <p className="text-3xl font-bold text-center mb-0">
-            Want To Become Best Selling Authors?
+          Do you want to become a best-selling author?
           </p>
-          <button className="text-white px-4 py-2 rounded-md bg-red-500 hover:bg-red-700 ml-2 hover:underline">
-            Contact Us
+          <button className="text-white px-4 py-2 rounded-md bg-red-500 hover:bg-red-700 ml-2 ">
+          <a href="https://wa.me/9999762706">Contact Us</a>
+            
           </button>
         </div>
         </>
