5a64601cee6e762d7361cddf2546dc0007bfff62
Yuvraj <kingyuvi88@gmail.com>
2023-11-16 17:49:27 +0530

    Added hover effect

--- src/components/Distributors.jsx
+++ src/components/Distributors.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import library from '../images/library.png'
+import library from '../images/library.png';
 
 const Distributors = () => {
-
   const distributorsImages = [
     '/brands/brand1.jpeg',
     '/brands/brand2.png',
@@ -14,21 +13,23 @@ const Distributors = () => {
     '/brands/brand8.png',
     '/brands/brand9.jpeg',
     '/brands/brand10.png',
-
     // Add more image URLs as needed
   ];
 
   return (
-    <div className="relative w-[90%] h-[70vh] flex flex-col items-center justify-center mx-auto mt-8 bg-cover bg-center rounded-lg shadow-md overflow-hidden" style={{ backgroundImage: 'url(../images/library.png)' }}>
-        <img className='absolute w-screen -z-10 h-screen blur-sm' src={library} alt="library"/>
+    <div
+      className="relative w-[90%] h-[70vh] flex flex-col items-center justify-center mx-auto mt-8 bg-cover bg-center rounded-lg shadow-md overflow-hidden "
+      style={{ backgroundImage: `url(${library})` }}
+    >
+      <img className="absolute w-screen -z-10 h-screen blur-sm" src={library} alt="library" />
       <h2 className="text-4xl font-bold text-white text-center mb-4">Our Distributors & Event Partners</h2>
       <div className="flex flex-wrap">
         {distributorsImages.map((image, index) => (
-          <div key={index} className="w-1/5 p-2">
+          <div key={index} className="w-1/5 p-2 hover:scale-110 transition-transform duration-300">
             <img
               src={image}
               alt={`Distributor ${index + 1}`}
-              className="w-full h-full object-cover rounded-lg"
+              className="w-full h-full object-cover rounded-lg hover:opacity-90 transition-opacity duration-300"
             />
           </div>
         ))}
