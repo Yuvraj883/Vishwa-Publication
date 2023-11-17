bd6e5c32cc9a44da7f3767158deb9279474ef5b3
Suraj Dubey <dubeysuraj864@gmail.com>
2023-11-17 07:16:38 +0530

    ui updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,9 +10,7 @@ import Foot from '../components/Foot';
 const About = () => {
   return (
     <div>
-      <Header></Header>
-      <Navbar></Navbar>
-      <CompanyAimSection />
+
       <FounderSection
         name="Mr. Vishwa Mohan Singh"
         description="Vishwa Mohan Singh is #1 Amazon Best Seller Author, Book Publishing Coach, educator, and business consultant. He is also the president of EFI, which is a group of entrepreneurs around the globe. His fan following consists mostly of those who lead others, rightfully so, since his passion is to help people reach their ultimate best as entrepreneurs and in their careers. His training sessions are spoken about with respect and awe. Almost all of his training sessions are filled with insights, high energy level, enthusiastic and enthralled participants. Vishwa Mohan Singh has been teaching and training authors and entrepreneurs for twenty years and has been a part of making many successful authors and entrepreneurs and helped many to achieve their goals. He has witnessed many success stories and knows the secrets of being a successful author, which all other best-selling authors have experimented with around the globe."
